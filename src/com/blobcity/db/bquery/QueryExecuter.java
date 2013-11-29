22b561f28720e477fe3f0b5c256a7844d84d5608
Sanket Sarang <sanket@blobcity.net>
2013-11-29 10:10:44 +0530

    Automatic operationmode detection logic perfected.

--- src/com/blobcity/db/bquery/QueryExecuter.java
+++ src/com/blobcity/db/bquery/QueryExecuter.java
@@ -10,6 +10,8 @@ import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.net.URLEncoder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import org.json.JSONObject;
@@ -22,18 +24,33 @@ import org.json.JSONObject;
 public class QueryExecuter {
 
     private static OperationMode operationMode = OperationMode.HTTP;
-    private static final String JNDI_RESOURCE = "java:global/TestEAR/TEjb/Calculator!com.CalculatorRemote";
+    private static final String JNDI_LOCAL_RESOURCE = "java:global/BlobCityDB/BQueryExecutorBean";
+    private static final String JNDI_REMOTE_RESOURCE = "java:global/BlobCityDB/BQueryExecutorBean!com.blobcity.db.bquery.BQueryExecutorBeanRemote";
     private static InitialContext context;
 
     static {
-        /* Detect mode of operation */
+        Logger.getLogger("BlobCity").log(Level.INFO, "Default operation mode is http");
+
+        /* Check for availability of local EJB */
         try {
             context = new InitialContext();
-
-            //TODO: Set appropriate JNDI resource URL
-            BQueryExecutorBeanRemote bean = (BQueryExecutorBeanRemote) context.lookup(JNDI_RESOURCE);
+            context.lookup(JNDI_LOCAL_RESOURCE);
+            operationMode = OperationMode.LOCAL_EJB;
+            Logger.getLogger("BlobCity").log(Level.INFO, "Operation mode set to no-query");
         } catch (NamingException ex) {
             //do nothing
+        }
+
+        /* Check for availability of remote EJB */
+        if (operationMode == OperationMode.HTTP) {
+            try {
+                context = new InitialContext();
+                BQueryExecutorBeanRemote bean = (BQueryExecutorBeanRemote) context.lookup(JNDI_REMOTE_RESOURCE);
+                operationMode = OperationMode.REMOTE_EJB;
+                Logger.getLogger("BlobCity").log(Level.INFO, "Operation mode set to remote-ejb");
+            } catch (NamingException ex) {
+                //do nothing
+            }
         }
     }
 
@@ -50,12 +67,15 @@ public class QueryExecuter {
     }
 
     private String executeLocalEJB(JSONObject jsonObject) {
-        throw new UnsupportedOperationException("Not yet supported.");
+        throw new UnsupportedOperationException("No-Query operation mode not supported in downloaded adapters. "
+                + "Feature only available to applications compiled on the BlobCity Cloud. If you have arrived at this "
+                + "operating mode it is very likely that you are hosting on BlobCity Cloud but did not compile your "
+                + "application on the BlobCity Cloud.");
     }
 
     private String executeRemoteEJB(JSONObject jsonObject) {
         try {
-            BQueryExecutorBeanRemote bean = (BQueryExecutorBeanRemote) context.lookup(JNDI_RESOURCE);
+            BQueryExecutorBeanRemote bean = (BQueryExecutorBeanRemote) context.lookup(JNDI_REMOTE_RESOURCE);
             return bean.runQuery(jsonObject.toString());
         } catch (NamingException ex) {
             throw new RuntimeException("Could not load database remote EJB for running database queries", ex);
