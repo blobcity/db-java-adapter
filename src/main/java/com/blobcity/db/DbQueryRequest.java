a41a692a39c17e5fa930753cd39d0f3df2213488
Sanket Sarang <sanket@blobcity.net>
2014-10-30 06:10:56 +0530

    Solved problem occurring due to presence of & in json data

--- src/main/java/com/blobcity/db/DbQueryRequest.java
+++ src/main/java/com/blobcity/db/DbQueryRequest.java
@@ -4,12 +4,18 @@
 package com.blobcity.db;
 
 import com.blobcity.db.config.Credentials;
+import com.blobcity.db.exceptions.DbOperationException;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.MessageFormat;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * Internal class to represent a query to be sent to the database
  *
  * @author Karun AB <karun.ab@blobcity.net>
+ * @author Sanket Sarang <sanket@blobcity.net>
  */
 class DbQueryRequest {
 
@@ -34,6 +40,11 @@ class DbQueryRequest {
     }
 
     public String createPostParam() {
-        return MessageFormat.format("username={0}&password={1}&db={2}&q={3}", credentials.getUsername(), credentials.getPassword(), credentials.getDb(), query);
+        try {
+            return MessageFormat.format("username={0}&password={1}&db={2}&q={3}", credentials.getUsername(), credentials.getPassword(), credentials.getDb(), URLEncoder.encode(query, "UTF-8"));
+        } catch (UnsupportedEncodingException ex) {
+            Logger.getLogger(DbQueryRequest.class.getName()).log(Level.SEVERE, null, ex);
+            throw new DbOperationException("UNKNOWN", "The requested data could not be encoded to UTF-8 format for transmission over network");
+        }
     }
 }
