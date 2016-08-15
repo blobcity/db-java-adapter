d623b4d636f4c3f40bfbf47606ef2afd4d308787
Sanket Sarang <sanket@blobcity.com>
2016-08-16 02:07:23 +0530

    Renamed TableType to CollectionType

--- src/main/java/com/blobcity/db/enums/CollectionType.java
+++ src/main/java/com/blobcity/db/enums/CollectionType.java
@@ -7,14 +7,14 @@ package com.blobcity.db.enums;
  *
  * @author Prikshit Kumar
  */
-public enum TableType {
+public enum CollectionType {
     
     ON_DISK("on-disk"),
     IN_MEMORY("in-memory"),
     IN_MEMORY_NON_DURABLE("in-memory-nd");
     private final String type;
 
-    TableType(final String type) {
+    CollectionType(final String type) {
         this.type = type;
     }
 
